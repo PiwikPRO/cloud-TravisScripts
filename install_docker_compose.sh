404a9b1b066b21bb8e2ee76e7dc4c401725a0545
Kamil Zajac <kamil@piwik.pro>
2017-07-12 12:01:50 +0000

    Script that is responsible for installation of docker-compose

---
+++ install_docker_compose.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -n "$DOCKER_COMPOSE_VERSION" ]; then
+  DOCKER_COMPOSE_VERSION="1.9.0"
+fi
+
+echo "Composer version to install: $DOCKER_COMPOSE_VERSION"
+
+sudo rm /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
+chmod +x docker-compose
+sudo mv docker-compose /usr/local/bin
