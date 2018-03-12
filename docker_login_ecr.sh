abf203726279c074c12404eb645f66f6eb8a1808
Bartosz Cisek <b.cisek@piwik.pro>
2018-03-12 13:24:13 +0100

    make sure we fail early and fast

--- docker_login_ecr.sh
+++ docker_login_ecr.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail 
+
 aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID?"empty aws_access_key"};
 aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY?"empty secret_access_key"};
 DOCKER_LOGIN_CMD=`aws ecr get-login --region ${AWS_REGION?"empty region"} --no-include-email`;
