a5f6cbb3e397413f0f1e13f3aa2de6e6b7576b10
Bartosz Cisek <b.cisek@piwik.pro>
2018-03-12 13:21:56 +0100

    validate aws environment variables

--- docker_login_ecr.sh
+++ docker_login_ecr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID;
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY;
-DOCKER_LOGIN_CMD=`aws ecr get-login --region $AWS_REGION --no-include-email`;
+aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID?"empty aws_access_key"};
+aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY?"empty secret_access_key"};
+DOCKER_LOGIN_CMD=`aws ecr get-login --region ${AWS_REGION?"empty region"} --no-include-email`;
 
 eval $DOCKER_LOGIN_CMD
