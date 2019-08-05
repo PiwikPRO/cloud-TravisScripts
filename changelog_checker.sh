2d66197bd5c3808dee423e2b929a9fc00bf65405
Marek Fijas <m.fijas@piwik.pro>
2019-08-05 08:52:02 +0200

    changelog checker common travis script

---
+++ changelog_checker.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "${TRAVIS_PULL_REQUEST_BRANCH}" ]; then
+    echo This is not a PR, skipping
+
+    exit
+fi
+
+printf "Checking CHANGELOG.md entries between ${TRAVIS_BRANCH} and ${TRAVIS_PULL_REQUEST_BRANCH}... "
+
+if git diff --diff-filter=AM HEAD..${TRAVIS_BRANCH} --exit-code -s -- CHANGELOG.md > /dev/null 2>&1 ; then
+    echo "FAILED!"
+    echo "Please update ./CHANGELOG.md file"
+
+    exit 1
+fi
+
+echo "SUCCESS!"
