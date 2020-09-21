4698c52b6ccc41fd24dcc0d60f2d693b71cd1a6f
jzyinq <j.zywert@piwik.pro>
2020-09-21 14:04:17 +0200

    PPCDEV-11991 semver check script

--- semver_check.sh
+++ semver_check.sh
@@ -4,7 +4,8 @@
 PATTERN='^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
 STRING=$1
 
-MATCH=$( echo $STRING | grep -P $PATTERN )
+MATCH=$( echo $STRING | egrep $PATTERN )
+
 if [ -z $MATCH ]; then
   echo "'$STRING' is not a valid semver version"
   exit 1
